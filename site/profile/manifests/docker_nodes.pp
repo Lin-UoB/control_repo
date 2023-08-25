fa5ac116e72eb1ea6921c723ff7e239259674926
Lin-UoB <53185681+Lin-UoB@users.noreply.github.com>
2023-08-25 16:38:34 +0100

    Update docker_nodes.pp

--- site/profile/manifests/docker_nodes.pp
+++ site/profile/manifests/docker_nodes.pp
@@ -1,6 +1,7 @@
 class profile::docker_nodes {
   include docker
-  docker::image { 'ubuntu':
-    image_tag => 'precise'
+  docker::run { 'helloworld':
+  image   => 'base',
+  command => '/bin/sh -c "while true; do echo hello world; sleep 1; done"',
   }
 }
