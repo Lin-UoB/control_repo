36b67c5d7839b2faa260672c8619735773777670
Lin-UoB <53185681+Lin-UoB@users.noreply.github.com>
2023-08-31 13:08:40 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,6 +7,9 @@ node 'master.puppet.vm' {
     content => " Welcome to ${fqdn}\n",
   }
 }
+node 'minetest.puppet.vm' {
+  include role::mincraft_server
+}
 
 node /^web/ {
   include role::app_server
