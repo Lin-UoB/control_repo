c6f61c290fd0ccc378838c92b0fbc54b90343c76
Lin-UoB <53185681+Lin-UoB@users.noreply.github.com>
2023-08-31 13:42:35 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -10,7 +10,7 @@ class minecraft {
     ensure => file,
     content => 'eula=true',
   }
-  file {'/etc/systemd/system/mincraft.service':
+  file {'/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
